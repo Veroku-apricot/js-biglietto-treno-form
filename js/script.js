5ef10dbc35e15b55b319c384c8fe46fcd55d706c
Veronika <veronikakuchtova95@gmail.com>
2020-10-21 16:49:12 +0200

    Added some variables

--- js/script.js
+++ js/script.js
@@ -1,18 +1,26 @@
-
-// Calc ticket price
-// Normal price
-var ticketPrice = (0.21 * km).toFixed(2);
-console.log("Normal price ticket: ", ticketPrice);
+// Variables
+var name = document.getElementById("name").value;
+var km = document.getElementById("km").value;
+var age = document.getElementById("age").value;
+var genera = document.getElementById("genera");
+var annulla = document.getElementById("annulla");
+  // Calc ticket price
+  // Normal price
+  var ticketPrice = (0.21 * km).toFixed(2);
+  console.log("Normal price ticket: ", ticketPrice);
+  // Discounted price
+  var priceJunior = (ticketPrice - (ticketPrice * 0.2)).toFixed(2);
+  var priceSenior = (ticketPrice - (ticketPrice * 0.4)).toFixed(2);
 
 // Output
-if (age < 18) {
-  // 20% discount
-  document.getElementById("title").innerHTML = "Congratulazioni! Per lei uno sconto del 20%!" + "<br>" + "Prezzo finale: " + (ticketPrice - (ticketPrice * 0.2)).toFixed(2) + "&euro;";
-  console.log("Discounted price for under 18: ", (ticketPrice - (ticketPrice * 0.2)).toFixed(2));
-} else if (age > 65){
-  // 40% discount
-  document.getElementById("title").innerHTML = "Congratulazioni! Per lei uno sconto del 40%!" + "<br>" + "Prezzo finale: " + (ticketPrice - (ticketPrice * 0.4)).toFixed(2) + "&euro;";
-  console.log("Discounted price for over 65: ", (ticketPrice - (ticketPrice * 0.4)).toFixed(2));
-} else {
-  document.getElementById("title").innerHTML = "Prezzo totale (Iva esclusa): " + ticketPrice + "&euro;";
-}
+genera.addEventListener('click',
+  function() {
+
+  }
+)
+
+annulla.addEventListener('click',
+  function() {
+
+  }
+)
